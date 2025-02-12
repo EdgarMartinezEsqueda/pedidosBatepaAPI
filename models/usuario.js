4181124bb8ab63b71a2e4afff2f5d837f544427e
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-02-12 15:51:03 -0600

    feat: add usuario model for user data structure

---
+++ models/usuario.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database"); // import the db conection
+
+const Usuario = sequelize.define( "usuarios", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    username: { type: DataTypes.STRING(256), allowNull: false },
+    email: { type: DataTypes.STRING(256), validate: { isEmail : true }, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING(256), allowNull: false },
+    rol: { type: DataTypes.ENUM("Direccion", "Almacen", "Ts"), default: "Almacen" },
+    verificado: { type: DataTypes.BOOLEAN, defaultValue: false }
+}, {} );
+
+module.exports = Usuario;
