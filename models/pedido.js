5e5057c3a77d217a7ae5404d65a7bc4bebf890cd
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-02-12 15:49:49 -0600

    feat: add pedido model for order data structure

---
+++ models/pedido.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Pedido = sequelize.define("pedidos", {
+    id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true },
+    createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    idTs: { type: DataTypes.INTEGER, references: { model: "Usuarios", key: "id" } },
+    idRuta: { type: DataTypes.INTEGER, references: { model: "Rutas", key: "id" } },
+    fechaEntrega: DataTypes.DATEONLY,
+    estado: { type: DataTypes.ENUM ("pendiente", "creado", "finalizado"), defaultValue: "creado" },
+    observaciones: DataTypes.TEXT,
+    devoluciones: DataTypes.INTEGER,
+});
+
+module.exports = Pedido;
