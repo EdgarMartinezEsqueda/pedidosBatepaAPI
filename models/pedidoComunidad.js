25c8b7ddff7e59dcf452e1303ce38c496cbffa24
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-02-12 15:50:10 -0600

    feat: add pedidoComunidad model for community-order relationships

---
+++ models/pedidoComunidad.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const PedidoComunidad = sequelize.define("pedidoComunidad", {
+    idPedido: { type: DataTypes.BIGINT, primaryKey: true, references: { model: "Pedidos",  key: "id"} },
+    idComunidad: { type: DataTypes.INTEGER, primaryKey: true, references: {  model: "Comunidades", key: "id" } },
+    despensasCosto: DataTypes.INTEGER,
+    despensasMedioCosto: DataTypes.INTEGER,
+    despensasSinCosto: DataTypes.INTEGER,
+    despensasApadrinadas: DataTypes.INTEGER,
+    arpilladas: DataTypes.BOOLEAN,
+});
+
+module.exports = PedidoComunidad;
