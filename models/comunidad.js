9ab9c2defba5e4afe8afa5e310593dc6a4fa5148
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-02-12 15:42:10 -0600

    feat: add comunidad model for community data structure

---
+++ models/comunidad.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Comunidad = sequelize.define("comunidades", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    nombre: { type: DataTypes.TEXT, allowNull: false },
+    jefa: DataTypes.TEXT,
+    contacto: DataTypes.TEXT,
+    direccion: DataTypes.TEXT,
+    idRuta: { type: DataTypes.INTEGER, references: { model: "Rutas", key: "id" } }
+});
+
+module.exports = Comunidad;
