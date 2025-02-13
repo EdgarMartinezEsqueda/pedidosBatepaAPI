7d73ae16d0602b32265952df616c210031b94927
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-02-13 08:42:20 -0600

    feat: add orders routes for handling order operations

---
+++ routes/orders.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const router = Router();
+const ctrlPedidos = require("../controllers/ctrlPedidos");
+const verify = require("../middleware/verifyToken");
+
+// Order endpoints
+router
+    .post("/", verify.verifyToken, ctrlPedidos.createOrder)
+    .get("/", verify.verifyToken, ctrlPedidos.getAllOrders)
+    .get("/:id", verify.verifyToken, ctrlPedidos.getOrder)
+    .patch("/:id", verify.verifyTokenAndAuthorization, ctrlPedidos.updateOrder)
+    .delete("/:id", verify.verifyTokenAndAdmin, ctrlPedidos.deleteOrder); 
+
+module.exports = router;
