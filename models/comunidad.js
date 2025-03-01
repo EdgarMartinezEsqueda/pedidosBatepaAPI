a2e05de631cb8f89f09b9ffa52346cb586d77226
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-03-01 09:45:24 -0600

    fix: Update comunidad model
    Now the model has a 'municipio' field

--- models/comunidad.js
+++ models/comunidad.js
@@ -4,6 +4,7 @@ const sequelize = require("../config/database");
 const Comunidad = sequelize.define("comunidades", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     nombre: { type: DataTypes.TEXT, allowNull: false },
+    municipio: { type: DataTypes.TEXT, allowNull: false },
     jefa: DataTypes.TEXT,
     contacto: DataTypes.TEXT,
     direccion: DataTypes.TEXT,
