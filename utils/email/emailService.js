c0c14296980c05265a500fc48a37e858a7f4d063
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-05-05 00:53:58 -0600

    feat: update emailService to send emails when the user selects to retrieve his password

--- utils/email/emailService.js
+++ utils/email/emailService.js
@@ -1,6 +1,7 @@
 const { Resend } = require("resend");
 const logger = require("../logger");
 const { generateVerificationTemplate } = require("./templates/verificationEmail");
+const { generatePasswordResetTemplate } = require("./templates/resetPassword")
 
 const resend = new Resend(process.env.TOKEN_RESEND);
 
@@ -22,4 +23,22 @@ const sendVerificationEmail = async (user) => {
     }
 };
 
-module.exports = { sendVerificationEmail };
+const sendPasswordResetEmail = async (user, token) => {
+    try {
+        const emailHTML = generatePasswordResetTemplate(token);
+        
+        await resend.emails.send({
+            from: "notificaciones@bamxtepatitlan.org",
+            to: user.email,
+            subject: "🔒 Restablece tu contraseña - BAMX Tepatitlán",
+            html: emailHTML
+        });
+        
+        logger.info(`Password reset email sent to: ${user.email}`);
+    } catch (error) {
+        logger.error("Error sending password reset email:", error);
+        throw error;
+    }
+};
+
+module.exports = { sendVerificationEmail, sendPasswordResetEmail };
