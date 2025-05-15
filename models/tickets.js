2e62bed3087fe894e16f8e0f412a22da9ccc467c
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-05-15 11:32:54 -0600

    feat: add tickets methods

---
+++ models/tickets.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Ticket = sequelize.define("ticket", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  idUsuario: { type: DataTypes.INTEGER, references: { model: "usuarios", key: "id" } },
+  estatus: { type: DataTypes.ENUM("abierto", "en_proceso", "cerrado", "cancelado"), defaultValue: "abierto" },
+  prioridad: { type: DataTypes.ENUM("baja", "media", "alta"), defaultValue: "baja" },
+  descripcion: { type: DataTypes.TEXT, allowNull: false }
+});
+
+module.exports = Ticket;
